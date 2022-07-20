612f36e1828ba19cff94e75eb836cba23a832b50
Zicheng <zichengshan@gmail.com>
2022-07-20 01:15:16 -0700

    Set up
    Spring Boot + Vaadin Flow

---
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -0,0 +1,18 @@
+package com.example.application.views.list;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("list")
+@Route(value = "")
+public class ListView extends VerticalLayout {
+
+    public ListView() {
+
+    }
+
+}
