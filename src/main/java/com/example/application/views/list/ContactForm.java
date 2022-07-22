300da895058a480359e9d81b72129b76e451488e
Zicheng <zichengshan@gmail.com>
2022-07-22 01:15:18 -0700

    Data binding and validation
    Bind inputs to a Contact object to create a fully functional form with validation

--- src/main/java/com/example/application/views/list/ContactForm.java
+++ src/main/java/com/example/application/views/list/ContactForm.java
@@ -1,8 +1,11 @@
 package com.example.application.views.list;
 
 import com.example.application.data.entity.Company;
+import com.example.application.data.entity.Contact;
 import com.example.application.data.entity.Status;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -10,6 +13,10 @@ import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.shared.Registration;
 
 import java.util.List;
 
@@ -17,6 +24,7 @@ public class ContactForm extends FormLayout {
     TextField firstName = new TextField("First Name");
     TextField lastName = new TextField("Last Name");
     TextField email = new TextField("Email");
+    Binder<Contact> binder = new BeanValidationBinder<>(Contact.class);
 
     ComboBox<Status> status = new ComboBox<Status>("Status");
     ComboBox<Company> company = new ComboBox<>("Company");
@@ -25,8 +33,18 @@ public class ContactForm extends FormLayout {
     Button delete = new Button("Delete");
     Button cancel = new Button("Cancel");
 
+    private Contact contact;
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+        // bind the values from the contact to the UI fields
+        binder.readBean(contact);
+    }
+
     public ContactForm(List<Company> companies, List<Status> statuses){
         addClassName("contact-form");
+        // Match fields in Contact and ContactForm based on their names.
+        binder.bindInstanceFields(this);
 
         company.setItems(companies);
         company.setItemLabelGenerator(Company::getName);
@@ -51,7 +69,61 @@ public class ContactForm extends FormLayout {
         save.addClickShortcut(Key.ENTER);
         cancel.addClickShortcut(Key.ESCAPE);
 
+        save.addClickListener(event -> validateAndSave());
+        delete.addClickListener(event -> fireEvent(new DeleteEvent(this, contact)));
+        cancel.addClickListener(event -> fireEvent(new CloseEvent(this)));
+
+        // Validates the form every time it changes.
+        binder.addStatusChangeListener(e -> save.setEnabled(binder.isValid()));
+
         return new HorizontalLayout(save, delete, cancel);
+    }
+
+    private void validateAndSave() {
+        try {
+            binder.writeBean(contact);
+            fireEvent(new SaveEvent(this, contact));
+        } catch (ValidationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Events
+    public static abstract class ContactFormEvent extends ComponentEvent<ContactForm> {
+        private Contact contact;
+
+        protected ContactFormEvent(ContactForm source, Contact contact) {
+            super(source, false);
+            this.contact = contact;
+        }
+
+        public Contact getContact() {
+            return contact;
+        }
+    }
+
+    public static class SaveEvent extends ContactFormEvent {
+        SaveEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+    }
+
+    public static class DeleteEvent extends ContactFormEvent {
+        DeleteEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+
+    }
+
+    public static class CloseEvent extends ContactFormEvent {
+        CloseEvent(ContactForm source) {
+            super(source, null);
+        }
+    }
+
+    public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType,
+                                                                  ComponentEventListener<T> listener) {
+        return getEventBus().addListener(eventType, listener);
     }
 
 }
